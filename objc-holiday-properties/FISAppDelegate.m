c128aee405beb597d813398d1de27ea18b03635b
SusanZheng15 <zheng.susan12@gmail.com>
2016-06-09 10:42:25 -0400

    Done

--- objc-holiday-properties/FISAppDelegate.m
+++ objc-holiday-properties/FISAppDelegate.m
@@ -4,45 +4,67 @@
 
 @implementation FISAppDelegate
 
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    //self.database;
     return YES;
 }
 
-- (NSArray *)holidaysInSeason:(NSString *)season {
-    
-    return nil;
+- (NSArray *)holidaysInSeason:(NSString *)season
+{
+    NSArray *holidays = [self.database[season] allKeys];
+    return holidays;
 }
 
 - (NSArray *)suppliesInHoliday:(NSString *)holiday
-                      inSeason:(NSString *)season {
-    
-    return nil;
+                      inSeason:(NSString *)season
+{
+    NSMutableArray *supplies = self.database[season][holiday];
+    return supplies;
 }
 
 - (BOOL)holiday:(NSString* )holiday
-     isInSeason:(NSString *)season {
+     isInSeason:(NSString *)season
+{
+    if ([self.database[season] objectForKey: holiday])
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
     
-    return nil;
 }
 
 - (BOOL)supply:(NSString *)supply
    isInHoliday:(NSString *)holiday
-      inSeason:(NSString *)season {
+      inSeason:(NSString *)season
+{
+    if ([self.database[season][holiday] containsObject:supply])
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
     
-    return nil;
 }
 
 - (void)addHoliday:(NSString *)holiday
-          toSeason:(NSString *)season {
-    
+          toSeason:(NSString *)season
+{
+    [self.database[season] setValue:@"" forKey:holiday];
     // no return
 }
 
 - (void)addSupply:(NSString *)supply
         toHoliday:(NSString *)holiday
-         inSeason:(NSString *)season {
-    
+         inSeason:(NSString *)season
+{
+    [self.database[season] setValue:supply forKey:holiday];
     // no return
 }
 
